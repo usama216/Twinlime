88530e863721420a68626cf2d73ca8f5a6e66ce4
dev016binarybrix <usamajatt16@gmail.com>
2025-02-17 13:21:45 +0500

    done

--- src/Pages/Landing/HeroSection.jsx
+++ src/Pages/Landing/HeroSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <section className=" h-screen flex flex-col items-center justify-center text-center text-white px-[10%] ">
+    <section className="h-[88vh] flex flex-col items-center justify-center text-center text-white px-[10%] ">
       {/* Background Video */}
       <div className="video-docker absolute top-0 left-0 w-full h-full overflow-hidden">
         <video
