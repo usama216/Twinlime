c9343c8c8d5fbd0b2cb13d68de04dd99dedb21c6
dev016binarybrix <99108743+usama216@users.noreply.github.com>
2025-02-26 13:29:58 +0500

    update with videos

--- src/Pages/Landing/Categories.jsx
+++ src/Pages/Landing/Categories.jsx
@@ -1,49 +1,163 @@
+// import AOS from "aos";
+// import "aos/dist/aos.css";
+// import { useEffect } from "react";
+
+// const cardData = [
+//   { id: 1, title: "Dairy", description: "City of love", imageUrl: "/All/Dairy.jpg" },
+//   { id: 2, title: "Confectionery", description: "The Big Apple", imageUrl: "/All/chocolate.jpg" },
+//   { id: 3, title: "Meat and Fish", description: "City of technology", imageUrl: "/All/meat.jpg" },
+//   { id: 4, title: "Jam and Sauces", description: "The city of history", imageUrl: "/All/Veg.jpg" },
+//   { id: 5, title: "Bakery", description: "The Eternal City", imageUrl: "/All/cake.jpg" },
+//   { id: 6, title: "Functional Additives", description: "The city of art", imageUrl: "/All/functional.png" },
+// ];
+
+// const Categories = () => {
+//   useEffect(() => {
+//     AOS.init({ duration: 1000, once: true });
+//   }, []);
+//   return (
+//     <>
+//       {/* <h1 className="text-4xl font-bold text-center underline" data-aos="fade-up">
+//         Categories
+//       </h1> */}
+//       <h2 className="text-3xl md:text-6xl font-extrabold mt-14 text-center text-[#614533]"
+//              data-aos="fade-up"
+
+// >
+//             Categories
+//           </h2>
+//       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 w-full gap-3 px-[10%] py-10">
+//         {/* Map through the cardData array to render the cards */}
+//         {cardData.map((card, index) => (
+//           <article
+//             key={card.id}
+//             className="relative isolate flex flex-col justify-end overflow-hidden w-full px-2 pb-8 pt-40 max-w-sm mx-auto mt-5"
+//             data-aos="fade-up"
+//             data-aos-delay={index * 100} // Staggered animation
+//           >
+//             <img src={card.imageUrl} alt={card.title} className="absolute inset-0 h-full w-full object-cover" />
+//             <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+//             <h3 className="z-10 mt-3 text-3xl ml-5 font-bold text-white">{card.title}</h3>
+//             <div className="z-10 mt-1 gap-y-1 ml-5 overflow-hidden text-sm leading-6 text-gray-300">
+//               <button className="border px-4 py-1 font-bold text-lg">Read more</button>
+//             </div>
+//           </article>
+//         ))}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Categories;
+
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 const cardData = [
-  { id: 1, title: "Dairy", description: "City of love", imageUrl: "/All/Dairy.jpg" },
-  { id: 2, title: "Confectionery", description: "The Big Apple", imageUrl: "/All/chocolate.jpg" },
-  { id: 3, title: "Meat and Fish", description: "City of technology", imageUrl: "/All/meat.jpg" },
-  { id: 4, title: "Jam and Sauces", description: "The city of history", imageUrl: "/All/Veg.jpg" },
-  { id: 5, title: "Bakery", description: "The Eternal City", imageUrl: "/All/cake.jpg" },
-  { id: 6, title: "Functional Additives", description: "The city of art", imageUrl: "/All/functional.png" },
+  { id: 1, title: "Dairy", videoUrl: "/video.mp4", imageUrl: "/All/Dairy.jpg" },
+  {
+    id: 2,
+    title: "Confectionery",
+    videoUrl: "/About/About-Video.mp4",
+    imageUrl: "/All/chocolate.jpg",
+  },
+  {
+    id: 3,
+    title: "Meat and Fish",
+    videoUrl: "/video.mp4",
+    imageUrl: "/All/meat.jpg",
+  },
+  {
+    id: 4,
+    title: "Jam and Sauces",
+    videoUrl: "/About/About-Video.mp4",
+    imageUrl: "/All/Veg.jpg",
+  },
+  {
+    id: 5,
+    title: "Bakery",
+    videoUrl: "/video.mp4",
+    imageUrl: "/All/cake.jpg",
+  },
+  {
+    id: 6,
+    title: "Functional Additives",
+    videoUrl: "/About/About-Video.mp4",
+    imageUrl: "/All/functional.png",
+  },
 ];
 
 const Categories = () => {
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
   useEffect(() => {
     AOS.init({ duration: 1000, once: true });
   }, []);
+
   return (
     <>
-      {/* <h1 className="text-4xl font-bold text-center underline" data-aos="fade-up">
+      <h2
+        className="text-3xl md:text-6xl font-extrabold mt-14 text-center text-[#614533]"
+        data-aos="fade-up"
+      >
         Categories
-      </h1> */}
-      <h2 className="text-3xl md:text-6xl font-extrabold mt-14 text-center text-[#614533]"
-             data-aos="fade-up"
-
->
-            Categories
-          </h2>
+      </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 w-full gap-3 px-[10%] py-10">
-        {/* Map through the cardData array to render the cards */}
         {cardData.map((card, index) => (
           <article
             key={card.id}
-            className="relative isolate flex flex-col justify-end overflow-hidden w-full px-2 pb-8 pt-40 max-w-sm mx-auto mt-5"
+            className="relative isolate flex flex-col justify-end overflow-hidden w-full px-2 pb-8 pt-40 max-w-sm mx-auto mt-5 cursor-pointer"
             data-aos="fade-up"
-            data-aos-delay={index * 100} // Staggered animation
+            data-aos-delay={index * 100}
+            onClick={() => setSelectedVideo(card.videoUrl)} // Set selected video on click
           >
-            <img src={card.imageUrl} alt={card.title} className="absolute inset-0 h-full w-full object-cover" />
+            <img
+              src={card.imageUrl}
+              alt={card.title}
+              className="absolute inset-0 h-full w-full object-cover"
+            />
             <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
-            <h3 className="z-10 mt-3 text-3xl ml-5 font-bold text-white">{card.title}</h3>
-            <div className="z-10 mt-1 gap-y-1 ml-5 overflow-hidden text-sm leading-6 text-gray-300">
-              <button className="border px-4 py-1 font-bold text-lg">Read more</button>
-            </div>
+            <h3 className="z-10 mt-3 text-3xl ml-5 font-bold text-white">
+              {card.title}
+            </h3>
           </article>
         ))}
       </div>
+
+      {/* Full-screen Video Modal */}
+      <AnimatePresence>
+        {selectedVideo && (
+          <motion.div
+            className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80 backdrop-blur-md z-50"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <motion.div
+              className="relative w-full h-full flex items-center justify-center"
+              initial={{ scale: 0.8 }}
+              animate={{ scale: 1 }}
+              exit={{ scale: 0.8 }}
+              transition={{ duration: 0.3 }}
+            >
+              <button
+                className="absolute top-5 right-5 text-white text-4xl font-bold l p-2 z-50"
+                onClick={() => setSelectedVideo(null)}
+              >
+                ✕
+              </button>
+              <video
+                src={selectedVideo}
+                controls
+                autoPlay
+                className="w-full h-auto shadow-lg rounded-lg"
+              ></video>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,184 @@
+// import React, { useState, useEffect } from "react";
+// import { Link, useNavigate } from "react-router-dom";
+// import { FaBars, FaTimes } from "react-icons/fa";
+// import Typewriter from "typewriter-effect";
+
+// const Navbar = () => {
+//   const [hovered, setHovered] = useState(null);
+//   const [isMenuOpen, setIsMenuOpen] = useState(false);
+//   const [isScrolled, setIsScrolled] = useState(false);
+//   const navigate = useNavigate();
+
+//   const [step, setStep] = useState(0);
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       setIsScrolled(window.scrollY > 50);
+//     };
+
+//     window.addEventListener("scroll", handleScroll);
+//     return () => window.removeEventListener("scroll", handleScroll);
+//   }, []);
+
+//   const menuItems = [
+//     { path: "/about-us", label: "About" },
+//     { path: "/food-industry", label: "Food Industry", hasCustomDropdown: true },
+//     { path: "#", label: "Products" },
+//     { path: "#", label: "Partners" },
+//     { path: "/contact", label: "Contact" },
+//     { path: "#", label: "Packaging Industry" },
+//   ];
+
+//   return (
+//     <>
+//       <nav
+//         className={`fixed top-0 left-0 w-full z-50 p-4 border-b transition-all duration-300 ${
+//           isScrolled ? "bg-black/50 shadow-lg" : "bg-transparent"
+//         }`}
+//       >
+//         <div className="flex items-center justify-between max-w-5xl mx-auto px-10">
+//           {/* Left Menu */}
+//           <div className="flex items-center space-x-10">
+//             {menuItems.slice(0, 3).map((item, index) => (
+//               <div
+//                 key={index}
+//                 className="relative"
+//                 onMouseEnter={() => setHovered(index)}
+//                 onMouseLeave={() => setHovered(null)}
+//               >
+//                 <Link to={item.path} className="text-white">
+//                   {item.label}
+//                 </Link>
+
+//                 {/* Custom Dropdown Layout for Food Industry */}
+//                 {hovered === index && item.hasCustomDropdown && (
+//                   <div className="absolute left-0  bg-white text-black shadow-lg flex w-[700px] border rounded-lg">
+//                     {/* Video Section */}
+//                     <div className="w-1/2 p-2 border-r">
+//                       <video
+//                         autoPlay
+//                         loop
+//                         muted
+//                         className="w-full h-full object-cover"
+//                       >
+//                         <source src="/video.mp4" type="video/mp4" />
+//                         Your browser does not support the video tag.
+//                       </video>
+//                     </div>
+
+//                     {/* Content Section */}
+
+//                     <div className="w-1/2 p-2">
+//                       <ul className="text-sm space-y-2">
+//                         <li className="font-bold text-xl">
+//                           <Typewriter
+//                             options={{
+//                               strings: ["• Rich tapestry of flavours"],
+//                               autoStart: true,
+//                               delay: 50,
+//                               cursor: "",
+//                             }}
+//                             onInit={(typewriter) => {
+//                               typewriter.callFunction(() => setStep(1)).start();
+//                             }}
+//                           />
+//                         </li>
+
+//                         {step >= 1 && (
+//                           <li className="font-bold text-xl">
+//                             <Typewriter
+//                               options={{
+//                                 strings: ["• High quality"],
+//                                 autoStart: true,
+//                                 delay: 50,
+//                                 cursor: "",
+//                               }}
+//                               onInit={(typewriter) => {
+//                                 typewriter
+//                                   .callFunction(() => setStep(2))
+//                                   .start();
+//                               }}
+//                             />
+//                           </li>
+//                         )}
+
+//                         {step >= 2 && (
+//                           <li className="font-bold text-xl">
+//                             <Typewriter
+//                               options={{
+//                                 strings: ["• Case-by-case approach"],
+//                                 autoStart: true,
+//                                 delay: 50,
+//                                 cursor: "",
+//                               }}
+//                             />
+//                           </li>
+//                         )}
+//                       </ul>
+//                     </div>
+//                     {/* <div className="w-1/2 p-2">
+//                       <ul className="text-sm space-y-2">
+//                         <li className="font-bold text-xl">• Rich tapestry of flavours</li>
+//                         <li className="font-bold text-xl">• High quality</li>
+//                         <li className="font-bold text-xl">• Case-by-case approach</li>
+//                       </ul>
+//                     </div> */}
+//                   </div>
+//                 )}
+//               </div>
+//             ))}
+//           </div>
+
+//           {/* Logo */}
+//           <div className="text-white font-bold spin3D">
+//             <img
+//               src="/twinlime-logo.png"
+//               className="w-[10rem] cursor-pointer"
+//               alt="Logo"
+//               onClick={() => navigate("/")}
+//             />
+//           </div>
+
+//           {/* Right Menu */}
+//           <div className="flex items-center space-x-10">
+//             {menuItems.slice(3).map((item, index) => (
+//               <div
+//                 key={index + 2}
+//                 className="relative"
+//                 onMouseEnter={() => setHovered(index + 2)}
+//                 onMouseLeave={() => setHovered(null)}
+//               >
+//                 <Link to={item.path} className="text-white">
+//                   {item.label}
+//                 </Link>
+//               </div>
+//             ))}
+//           </div>
+//         </div>
+//       </nav>
+//     </>
+//   );
+// };
+
+// export default Navbar;
+
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 import Typewriter from "typewriter-effect";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Navbar = () => {
   const [hovered, setHovered] = useState(null);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
   const navigate = useNavigate();
-
   const [step, setStep] = useState(0);
 
   useEffect(() => {
     const handleScroll = () => {
       setIsScrolled(window.scrollY > 50);
     };
-
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
@@ -31,8 +194,9 @@ const Navbar = () => {
 
   return (
     <>
+      {/* NAVBAR FOR LARGE SCREENS */}
       <nav
-        className={`fixed top-0 left-0 w-full z-50 p-4 border-b transition-all duration-300 ${
+        className={`fixed top-0 left-0 w-full z-50 p-4 border-b transition-all duration-300 hidden lg:block ${
           isScrolled ? "bg-black/50 shadow-lg" : "bg-transparent"
         }`}
       >
@@ -52,22 +216,16 @@ const Navbar = () => {
 
                 {/* Custom Dropdown Layout for Food Industry */}
                 {hovered === index && item.hasCustomDropdown && (
-                  <div className="absolute left-0  bg-white text-black shadow-lg flex w-[700px] border rounded-lg">
+                  <div className="absolute left-0 bg-white text-black shadow-lg flex w-[700px] border rounded-lg">
                     {/* Video Section */}
                     <div className="w-1/2 p-2 border-r">
-                      <video
-                        autoPlay
-                        loop
-                        muted
-                        className="w-full h-full object-cover"
-                      >
+                      <video autoPlay loop muted className="w-full h-full object-cover">
                         <source src="/video.mp4" type="video/mp4" />
                         Your browser does not support the video tag.
                       </video>
                     </div>
 
                     {/* Content Section */}
-
                     <div className="w-1/2 p-2">
                       <ul className="text-sm space-y-2">
                         <li className="font-bold text-xl">
@@ -78,9 +236,7 @@ const Navbar = () => {
                               delay: 50,
                               cursor: "",
                             }}
-                            onInit={(typewriter) => {
-                              typewriter.callFunction(() => setStep(1)).start();
-                            }}
+                            onInit={(typewriter) => typewriter.callFunction(() => setStep(1)).start()}
                           />
                         </li>
 
@@ -93,11 +249,7 @@ const Navbar = () => {
                                 delay: 50,
                                 cursor: "",
                               }}
-                              onInit={(typewriter) => {
-                                typewriter
-                                  .callFunction(() => setStep(2))
-                                  .start();
-                              }}
+                              onInit={(typewriter) => typewriter.callFunction(() => setStep(2)).start()}
                             />
                           </li>
                         )}
@@ -116,13 +268,6 @@ const Navbar = () => {
                         )}
                       </ul>
                     </div>
-                    {/* <div className="w-1/2 p-2">
-                      <ul className="text-sm space-y-2">
-                        <li className="font-bold text-xl">• Rich tapestry of flavours</li>
-                        <li className="font-bold text-xl">• High quality</li>
-                        <li className="font-bold text-xl">• Case-by-case approach</li>
-                      </ul>
-                    </div> */}
                   </div>
                 )}
               </div>
@@ -155,6 +300,48 @@ const Navbar = () => {
             ))}
           </div>
         </div>
+      </nav>
+
+      {/* NAVBAR FOR SMALL SCREENS */}
+      <nav className="fixed top-0 left-0 w-full z-50 p-4 border-b bg-black/50 shadow-lg lg:hidden">
+        <div className="flex items-center justify-between px-6">
+          {/* Logo */}
+          <img
+            src="/twinlime-logo.png"
+            className="w-[8rem] cursor-pointer"
+            alt="Logo"
+            onClick={() => navigate("/")}
+          />
+
+          {/* Menu Button */}
+          <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="text-white text-2xl z-50">
+            {isMenuOpen ? <FaTimes /> : <FaBars />}
+          </button>
+        </div>
+
+        {/* Mobile Menu (Slide-in effect) */}
+        <AnimatePresence>
+          {isMenuOpen && (
+            <motion.div
+              className="fixed inset-0 bg-black/90 z-40 flex flex-col items-center justify-center text-white text-2xl space-y-6"
+              initial={{ x: "100%" }}
+              animate={{ x: 0 }}
+              exit={{ x: "100%" }}
+              transition={{ duration: 0.3 }}
+            >
+              {menuItems.map((item, index) => (
+                <Link
+                  key={index}
+                  to={item.path}
+                  className="hover:underline"
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  {item.label}
+                </Link>
+              ))}
+            </motion.div>
+          )}
+        </AnimatePresence>
       </nav>
     </>
   );
